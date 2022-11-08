fec8c57ba0793bc5c6abdfff294a4dc1f31db815
Carl Bondin <carl.bondin@gmail.com>
2022-11-08 20:12:47 +0100

    Task 1: Amendment when searching in Maltapark

--- src/test/java/test/AssignmentPart1/Task1Tests/BrowserTests.java
+++ src/test/java/test/AssignmentPart1/Task1Tests/BrowserTests.java
@@ -10,6 +10,8 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
 import java.io.IOException;
 
 public class BrowserTests {
@@ -25,8 +27,12 @@ public class BrowserTests {
 
         //Navigating to Maltapark website
         driver.get("https://www.maltapark.com/");
-        WebElement searchField = driver.findElement(By.id("search"));
-        searchField.sendKeys("BMW");
+        //Navigating to Cars Category by hovering on Cars & Parts hoverable menu
+        Actions actions = new Actions(driver);
+        WebElement primarySearchCategory = driver.findElement(By.xpath("/html/body/div/header/div[2]/div/div/ul/li[4]/a"));
+        actions.moveToElement(primarySearchCategory).perform();
+        WebElement searchCategory = driver.findElement(By.xpath("/html/body/div/header/div[2]/div/div/ul/li[4]/ul/li[2]/a"));
+        searchCategory.click();
     }
 
     @AfterEach
@@ -36,49 +42,52 @@ public class BrowserTests {
 
     @Test
     public void testMaltaparkSearch(){
+        //Testing title of tab
         String title = driver.getTitle();
-        Assertions.assertEquals("Home | Maltapark", title);
+        Assertions.assertEquals("Cars | Maltapark", title);
     }
 
     @Test
     public void testAddItemToMyAlerts() throws IOException, NumberFormatException{
-        String itemCheck = driver.findElement(By.xpath("//div[@id='page-content-left']/div/@class")).toString();
-        System.out.println(itemCheck);
-        if(!(itemCheck.contains("item featured"))) {
-            //Getting item information
-            String url = driver.findElement(By.xpath("//a[@class='header']")).getAttribute("href");
-            driver.get(url);
-            System.out.println(url);
-
-            String heading = driver.findElement(By.xpath("//h1[@class='top-title']")).getText();
-            System.out.println(heading);
-
-            String imageUrl = driver.findElement(By.xpath("//a[@class = 'fancybox']")).getAttribute("href");
-            System.out.println(imageUrl);
-
-            String description = driver.findElement(By.xpath("//div[@class='readmore-wrapper']")).getText();
-            System.out.println(description);
-
-            String priceString = driver.findElement(By.xpath("//h1[@class='top-price']")).getText();
-            priceString = priceString.replace("€ ", "");
-            if (priceString.contains(",")) {
-                priceString = priceString.replace(",", "");
-            }
-            int price = Integer.parseInt(priceString);
-            System.out.println(price);
-            int priceCents = price * 100;
-            System.out.println(priceCents);
-
-            call.postRequest(
-                    1,
-                    heading,
-                    description,
-                    url,
-                    imageUrl,
-                    "46aba3d5-35a9-4850-b5c1-02824284c450",
-                    priceCents
-            );
+        //Testing the addition of one item to My Alerts
+        //Getting item information
+        String url = driver.findElement(By.xpath("//a[@class='header']")).getAttribute("href");
+        driver.get(url);
+        String title = driver.findElement(By.xpath("//h1[@class='top-title']")).getText();
+        String imageUrl = driver.findElement(By.xpath("//a[@class = 'fancybox']")).getAttribute("href");
+        String description = driver.findElement(By.xpath("//div[@class='readmore-wrapper']")).getText();
+        String priceString = driver.findElement(By.xpath("//h1[@class='top-price']")).getText();
+        priceString = priceString.replace("€ ", "");
+        if (priceString.contains(",")) {
+            priceString = priceString.replace(",", "");
         }
+        int price = Integer.parseInt(priceString);
+        int priceCents = price * 100;
+
+        //Logging of details
+        System.out.println("URL: " + url);
+        System.out.println("Title: " + title);
+        System.out.println("Image URL: " + imageUrl);
+        System.out.println("Description: " + description);
+        System.out.println("Price in cents: " + priceCents);
+
+        //Calling REST API
+        call.postRequest(
+                1,
+                title,
+                description,
+                url,
+                imageUrl,
+                "46aba3d5-35a9-4850-b5c1-02824284c450",
+                priceCents
+        );
+
+
+    }
+
+    @Test
+    public void testDeleteAllItems() throws IOException {
+        call.deleteRequest("46aba3d5-35a9-4850-b5c1-02824284c450");
     }
 }
 
