2fd47a13d0cb242c3209547520fd8e28d8aabd8c
Carl Bondin <carl.bondin@gmail.com>
2022-11-09 15:09:54 +0100

    Task1: Finalised Screen scraper + Updated tests

--- src/main/java/org/AssignmentPart1/Task1.java
+++ src/main/java/org/AssignmentPart1/Task1.java
@@ -6,69 +6,126 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.json.simple.JSONObject;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class Task1 {
-    public int postRequest(
-        int alertType,
-        String heading,
-        String description,
-        String url,
-        String imageURL,
-        String postedBy,
-        long priceInCents
-    ) throws IOException {
-        //Setting up HTTP request connection + headers
-        URL endpoint = new URL("https://api.marketalertum.com/Alert");
-        HttpURLConnection con = (HttpURLConnection) endpoint.openConnection();
-        con.setRequestMethod("POST");
-        con.setRequestProperty("Content-Type", "application/json");
-        con.setRequestProperty("Accept", "application/json");
-        con.setDoOutput(true);
+    WebDriver driver;
 
-        //Body
-        JSONObject body = new JSONObject();
-        body.put("alertType",alertType);
-        body.put("heading",heading);
-        body.put("description",description);
-        body.put("url",url);
-        body.put("imageURL",imageURL);
-        body.put("postedBy",postedBy);
-        body.put("priceInCents",priceInCents);
+    public List<Integer> screenScraper() throws IOException {
+        //Navigating to car category in maltapark
+        navigation();
+        List<Integer> statusCodes = postRequest(5);
+        return statusCodes;
+    }
 
-        String stringBody = body.toString();
-        try(OutputStream os = con.getOutputStream()) {
-            byte[] input = stringBody.getBytes("utf-8");
-            os.write(input, 0, input.length);
-        }
-        try(BufferedReader br = new BufferedReader(
-                new InputStreamReader(con.getInputStream(), "utf-8"))) {
-            StringBuilder response = new StringBuilder();
-            String responseLine = null;
-            while ((responseLine = br.readLine()) != null) {
-                response.append(responseLine.trim());
+    public String navigation(){
+        System.setProperty("webdriver.chrome.driver", "/Documents/UM/3rd year Sem1/CPS3230-Software Testing/Exercises/chromedriver_win32/chromedriver.exe");
+        driver = new ChromeDriver();
+
+        //Navigating to Maltapark website
+        driver.get("https://www.maltapark.com/");
+        //Navigating to Cars Category by hovering on Cars & Parts hoverable menu
+        Actions actions = new Actions(driver);
+        WebElement primarySearchCategory = driver.findElement(By.xpath("/html/body/div/header/div[2]/div/div/ul/li[4]/a"));
+        actions.moveToElement(primarySearchCategory).perform();
+        WebElement searchCategory = driver.findElement(By.xpath("/html/body/div/header/div[2]/div/div/ul/li[4]/ul/li[2]/a"));
+        searchCategory.click();
+
+        //Will be used in testing
+        return driver.getTitle();
+    }
+
+    public List<Integer> postRequest(int noOfAlerts) throws IOException {
+        //List containing all status codes of every POST request
+        List<Integer> statusCodes = new ArrayList<Integer>();
+        for(int i = 0; i < noOfAlerts; i++){
+            //Setting up HTTP request connection + headers
+            URL endpoint = new URL("https://api.marketalertum.com/Alert");
+            HttpURLConnection con = (HttpURLConnection) endpoint.openConnection();
+            con.setRequestMethod("POST");
+            con.setRequestProperty("Content-Type", "application/json");
+            con.setRequestProperty("Accept", "application/json");
+            con.setDoOutput(true);
+
+            //Getting Item Details
+            String url = driver.findElement(By.xpath("//a[@class='header']")).getAttribute("href");
+            driver.get(url);
+            String title = driver.findElement(By.xpath("//h1[@class='top-title']")).getText();
+            String imageUrl = driver.findElement(By.xpath("//a[@class = 'fancybox']")).getAttribute("href");
+            String description = driver.findElement(By.xpath("//div[@class='readmore-wrapper']")).getText();
+            String priceString = driver.findElement(By.xpath("//h1[@class='top-price']")).getText();
+            priceString = priceString.replace("€ ", "");
+            if (priceString.contains(",")) {
+                priceString = priceString.replace(",", "");
             }
-            System.out.println(response.toString());
-            return con.getResponseCode();
+            int price = Integer.parseInt(priceString);
+            int priceCents = price * 100;
+
+            //Logging of details
+            System.out.println("URL: " + url);
+            System.out.println("Title: " + title);
+            System.out.println("Image URL: " + imageUrl);
+            System.out.println("Description: " + description);
+            System.out.println("Price in cents: " + priceCents);
+
+            //Body
+            JSONObject body = new JSONObject();
+            body.put("alertType",1);
+            body.put("heading",title);
+            body.put("description",description);
+            body.put("url",url);
+            body.put("imageURL",imageUrl);
+            body.put("postedBy","46aba3d5-35a9-4850-b5c1-02824284c450");
+            body.put("priceInCents",priceCents);
+
+            String stringBody = body.toString();
+            try(OutputStream os = con.getOutputStream()) {
+                byte[] input = stringBody.getBytes("utf-8");
+                os.write(input, 0, input.length);
+            }
+            try(BufferedReader br = new BufferedReader(
+                    new InputStreamReader(con.getInputStream(), "utf-8"))) {
+                StringBuilder response = new StringBuilder();
+                String responseLine = null;
+                while ((responseLine = br.readLine()) != null) {
+                    response.append(responseLine.trim());
+                }
+                System.out.println(response.toString());
+            }
+            Integer statusCode = Integer.valueOf(con.getResponseCode());
+            statusCodes.add(statusCode);
         }
+        driver.quit();
+        return statusCodes;
     }
 
     public int deleteRequest(String userId) throws IOException{
+        System.setProperty("webdriver.chrome.driver", "/Documents/UM/3rd year Sem1/CPS3230-Software Testing/Exercises/chromedriver_win32/chromedriver.exe");
+        driver = new ChromeDriver();
+
         URL endpoint = new URL("https://api.marketalertum.com/Alert?userId=" + userId);
         HttpURLConnection con = (HttpURLConnection) endpoint.openConnection();
         con.setRequestMethod("DELETE");
         con.setRequestProperty("Accept", "application/json");
         con.setDoOutput(false);
 
-        try(BufferedReader br = new BufferedReader(
-                new InputStreamReader(con.getInputStream(), "utf-8"))) {
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(),"utf-8"))) {
             StringBuilder response = new StringBuilder();
             String responseLine = null;
             while ((responseLine = br.readLine()) != null) {
                 response.append(responseLine.trim());
             }
+
             System.out.println(response.toString());
+            driver.quit();
             return con.getResponseCode();
         }
     }
--- src/test/java/test/AssignmentPart1/Task1Tests/BrowserTests.java
+++ src/test/java/test/AssignmentPart1/Task1Tests/BrowserTests.java
@@ -5,89 +5,49 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BrowserTests {
-    WebDriver driver;
-    Task1 call;
+    Task1 task1;
 
     @BeforeEach
     public void setup() {
-        call = new Task1();
-
-        System.setProperty("webdriver.chrome.driver", "/Documents/UM/3rd year Sem1/CPS3230-Software Testing/Exercises/chromedriver_win32/chromedriver.exe");
-        driver = new ChromeDriver();
-
-        //Navigating to Maltapark website
-        driver.get("https://www.maltapark.com/");
-        //Navigating to Cars Category by hovering on Cars & Parts hoverable menu
-        Actions actions = new Actions(driver);
-        WebElement primarySearchCategory = driver.findElement(By.xpath("/html/body/div/header/div[2]/div/div/ul/li[4]/a"));
-        actions.moveToElement(primarySearchCategory).perform();
-        WebElement searchCategory = driver.findElement(By.xpath("/html/body/div/header/div[2]/div/div/ul/li[4]/ul/li[2]/a"));
-        searchCategory.click();
-    }
-
-    @AfterEach
-    public void teardown() {
-        driver.quit();
+        task1 = new Task1();
     }
 
     @Test
-    public void testMaltaparkSearch(){
+    public void testMaltaparkNavigation(){
         //Testing title of tab
-        String title = driver.getTitle();
+        String title = task1.navigation();
         Assertions.assertEquals("Cars | Maltapark", title);
+    }
+
+    @Test
+    public void testAdd5ItemsToMyAlerts() throws IOException {
+        List<Integer> statusCodes = task1.screenScraper();
+        boolean check = statusCodes.contains(201);
+        Assertions.assertTrue(check);
     }
 
     @Test
     public void testAddItemToMyAlerts() throws IOException, NumberFormatException{
         //Testing the addition of one item to My Alerts
 
-        //Getting item information
-        String url = driver.findElement(By.xpath("//a[@class='header']")).getAttribute("href");
-        driver.get(url);
-        String title = driver.findElement(By.xpath("//h1[@class='top-title']")).getText();
-        String imageUrl = driver.findElement(By.xpath("//a[@class = 'fancybox']")).getAttribute("href");
-        String description = driver.findElement(By.xpath("//div[@class='readmore-wrapper']")).getText();
-        String priceString = driver.findElement(By.xpath("//h1[@class='top-price']")).getText();
-        priceString = priceString.replace("€ ", "");
-        if (priceString.contains(",")) {
-            priceString = priceString.replace(",", "");
-        }
-        int price = Integer.parseInt(priceString);
-        int priceCents = price * 100;
-
-        //Logging of details
-        System.out.println("URL: " + url);
-        System.out.println("Title: " + title);
-        System.out.println("Image URL: " + imageUrl);
-        System.out.println("Description: " + description);
-        System.out.println("Price in cents: " + priceCents);
+        //Navigating to car listings page
+        task1.navigation();
 
         //Calling REST API
-        int statusCode = call.postRequest(
-                1,
-                title,
-                description,
-                url,
-                imageUrl,
-                "46aba3d5-35a9-4850-b5c1-02824284c450",
-                priceCents
-        );
-
-        Assertions.assertEquals(201,statusCode);
+        List<Integer> statusCodes = task1.postRequest(1);
+        boolean check = statusCodes.contains(201);
+        Assertions.assertTrue(check);
     }
 
     @Test
     public void testDeleteAllItems() throws IOException {
         //Calling REST API
-        int statusCode = call.deleteRequest("46aba3d5-35a9-4850-b5c1-02824284c450");
+        int statusCode = task1.deleteRequest("46aba3d5-35a9-4850-b5c1-02824284c450");
         Assertions.assertEquals(200,statusCode);
     }
 }
